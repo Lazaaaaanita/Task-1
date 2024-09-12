353b9b8f84f5c7bd9e283e513cc4415274976acf
lazanita <157325135+lazanita@users.noreply.github.com>
2024-09-12 04:10:21 +0300

    hibernate realization

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class Util {
-    // реализуйте настройку соеденения с БД
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "fralazan123";
+
     public static Connection getConection() {
         Connection connection = null;
         try {
@@ -17,6 +25,33 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+    private static StandardServiceRegistry standardServiceRegistry;
+    private static SessionFactory sessionFactory;
+    static {
+        try {
+            if (sessionFactory== null) {
 
+                StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+                standardServiceRegistryBuilder.loadProperties("hibernate.properties");
+
+                standardServiceRegistry = standardServiceRegistryBuilder.build();
+
+                MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+                metadataSources.addAnnotatedClass(User.class);
+
+                Metadata metadata = metadataSources.getMetadataBuilder().build();
+
+                sessionFactory=metadata.getSessionFactoryBuilder().build();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (standardServiceRegistry != null) {
+                StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
+            }
+        }
+    }
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
     }
 }
